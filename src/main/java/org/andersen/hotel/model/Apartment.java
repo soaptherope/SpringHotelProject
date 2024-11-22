f1a3a8ec8942e6c3f6eb99d80bcffb4f0753f028
youcanwakemeup <127168034+youcanwakemeup@users.noreply.github.com>
2024-11-23 03:38:41 +0500

    add entities, liquibase script

---
+++ src/main/java/org/andersen/hotel/model/Apartment.java
@@ -0,0 +1,37 @@
+package org.andersen.hotel.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "apartments")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Apartment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "apartment_status", nullable = false)
+    private ApartmentStatusEnum apartmentStatus;
+
+    @Column(name = "client_name", length = 40)
+    private String nameOfClient;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    public Apartment(double price) {
+        this.price = price;
+        this.apartmentStatus = ApartmentStatusEnum.FREE;
+    }
+}
