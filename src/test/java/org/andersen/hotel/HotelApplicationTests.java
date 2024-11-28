c37cf292d01cba05ef9c05e50842d953386d184f
youcanwakemeup <127168034+youcanwakemeup@users.noreply.github.com>
2024-11-28 23:24:38 +0500

    fix dependencies

--- src/test/java/org/andersen/hotel/HotelApplicationTests.java
+++ src/test/java/org/andersen/hotel/HotelApplicationTests.java
@@ -3,7 +3,7 @@ package org.andersen.hotel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = HotelApplication.class)
+@SpringBootTest
 class HotelApplicationTests {
 
     @Test
